770baf1f53102f1199c25e5b49a6ffd388710a35
Billy <william.allan@glasgow.ac.uk>
2018-06-06 13:50:07 +0100

    WIP

--- src/LdapConnection.php
+++ src/LdapConnection.php
@@ -42,7 +42,6 @@ class LdapConnection implements LdapConnectionInterface
         // if (!$info) {
         //     return false;
         // }
-        dd($username, $password);
         return @ldap_bind($this->ldap, $username, $password);
     }
 
